461b307fecd4bec0b3520f32b6c91c9c32efb7a3
Braian-MancioniBoggon <104107377+Braian-MancioniBoggon@users.noreply.github.com>
2022-04-22 19:57:23 -0300

    Agregado el ejercicio 1 Se creó el archivo "ejercicio1.ts" dentro de la carpeta "src" con los 10 pasos para hacer una torta

---
+++ src/ejercicio1.ts
@@ -0,0 +1,10 @@
+console.log("01 Seleccione las cacerolas a utilizar");
+console.log("02 Engrasar y precalentar");
+console.log("03 Prepare sus ingredientes");
+console.log("04 Batir la mezcla seca");
+console.log("05 Crema tu mantequilla y azúcar");
+console.log("06 Agregue los huevos");
+console.log("07 Es hora de combinar");
+console.log("08 Vierta su masa en su sartén");
+console.log("09 Hornee y enfríe sus pasteles");
+console.log("10 Decore sus pasteles a gusto");
