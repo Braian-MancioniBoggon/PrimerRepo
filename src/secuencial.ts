1b72b1ec5ab40fb484dad7f8de9d1ef79f29b8cf
Braian-MancioniBoggon <104107377+Braian-MancioniBoggon@users.noreply.github.com>
2022-04-25 21:28:15 -0300

    Secuencial agregado el ejercicio del algoritmo secuencial con variables

---
+++ src/secuencial.ts
@@ -0,0 +1,13 @@
+let palabra1: string = "Este";
+let palabra2: string = "algoritmo";
+let palabra3: string = "es";
+let palabra4: string = "secuencial";
+/* este programa imprime 4 líneas por la consola,
+Además tiene comentarios */
+console.log(palabra1);
+//esto es un comentario
+console.log(palabra2);
+//esto es otro comentario
+console.log(palabra3);
+//esto es un cuarto comentario
+console.log(palabra4);
